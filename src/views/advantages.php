075a88619dfa4d268df5317bd93fb01e54e35e45
ifreeworklancer <ifreeworklancer@gmail.com>
2018-12-26 15:24:08 +0200

    edit_thanks_and_block

---
+++ src/views/advantages.php
@@ -0,0 +1,51 @@
+<section id="advantages">
+	<div class="container">
+		<div class="row justify-content-around pt-5">
+			<div class="col-sm-6">
+				<div class="advantages-item">
+					<h2 class="section-title section-title--primary section-title--advantages text-dark">
+						Потеряли мотивацию развиваться и двигаться дальше? Хотите вернуть драйв в свою жизнь и бизнес, обрести эффективное окружение?
+					</h2>
+				</div>
+			</div>
+			<div class="col-sm-5">
+				<div class="advantages-img advantages-img--right"
+					 style="background-image: url('images/content/advantages/advantages-item-1.jpg');"></div>
+			</div>
+			<div class="col-sm-5">
+				<div class="advantages-img advantages-img--left"
+					 style="background-image: url('images/content/advantages/advantages-item-2.jpg');"></div>
+			</div>
+			<div class="col-sm-6">
+				<div class="advantages-item advantages-item--light">
+					<h2 class="section-title section-title--secondary text-dark">
+						Есть потенциал увеличить доход, но вы закопались в рутине? Хотите вернуть творчество в свою деятельность, реализовать новые проекты?
+					</h2>
+				</div>
+			</div>
+			<div class="col-sm-6">
+				<div class="advantages-item">
+					<h2 class="section-title section-title--primary text-dark">
+						Хотите получать кайф, осуществлять всё то, что хотели, но постоянно себе отказывали? Ждать новый день, а не когда он закончится?
+					</h2>
+				</div>
+			</div>
+			<div class="col-sm-5">
+				<div class="advantages-img advantages-img--right"
+					 style="background-image: url('images/content/advantages/advantages-item-3.jpg');"></div>
+			</div>
+			<div class="col-sm-5">
+				<div class="advantages-img advantages-img--left"
+					 style="background-image: url('images/content/advantages/advantages-item-4.jpg');"></div>
+			</div>
+			<div class="col-sm-6">
+				<div class="advantages-item advantages-item--light">
+					<h2 class="section-title section-title--secondary text-dark">
+						Вам страшно кардинально что-то менять? Находитесь на грани эмоционального выгорания от постоянных перегрузок и стресса?
+					</h2>
+				</div>
+			</div>
+		</div>
+		<div class="decor-line decor-line--advantages"></div>
+	</div>
+</section>
